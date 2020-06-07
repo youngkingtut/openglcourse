50c97dbf3840071bf04b151e6c8f6b4fafc83f5e
youngkingtut <tristanstorz@gmail.com>
2020-06-07 15:43:05 -0600

    3.19 complete.

--- src/Window.cpp
+++ src/Window.cpp
@@ -5,9 +5,10 @@ Window::Window(): Window(800, 600) {}
 Window::Window(GLint windowWidth, GLint windowHeight) {
     width = windowWidth;
     height = windowHeight;
-    bufferHeight = 0;
-    bufferWidth = 0;
-    window = nullptr;
+}
+
+Window::~Window() {
+    glfwDestroyWindow(window);
 }
 
 void Window::initialize() {
@@ -37,6 +38,10 @@ void Window::initialize() {
     // Set context for GLEW to use
     glfwMakeContextCurrent(window);
 
+    // Handle key and mouse input
+    createCallbacks();
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
     // Initialize GLEW (using experimental features)
     glewExperimental = GL_TRUE;
     if(glewInit() != GLEW_OK) {
@@ -51,8 +56,55 @@ void Window::initialize() {
 
     // Setup viewport size
     glViewport(0, 0, bufferWidth, bufferHeight);
+
+    glfwSetWindowUserPointer(window, this);
 }
 
-Window::~Window() {
-    glfwDestroyWindow(window);
+void Window::createCallbacks() {
+    glfwSetKeyCallback(window, handleKeys);
+    glfwSetCursorPosCallback(window, handleMouse);
+}
+
+void Window::handleKeys(GLFWwindow *window, int key, int code, int action, int mode) {
+    auto theWindow = static_cast<Window*>(glfwGetWindowUserPointer(window));
+
+    if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+
+    if(key >= 0 && key < 1024) {
+        if(action == GLFW_PRESS) {
+            theWindow->keys[key] = true;
+        } else if (action == GLFW_RELEASE) {
+            theWindow->keys[key] = false;
+        }
+    }
+}
+
+void Window::handleMouse(GLFWwindow *window, double xPosition, double yPosition) {
+    auto theWindow = static_cast<Window*>(glfwGetWindowUserPointer(window));
+
+    if(theWindow->mouseFirstMove) {
+        theWindow->lastX = xPosition;
+        theWindow->lastY = yPosition;
+        theWindow->mouseFirstMove = false;
+    }
+
+    theWindow->xChange = xPosition - theWindow->lastX;
+    theWindow->yChange = theWindow->lastY - yPosition;
+
+    theWindow->lastX = xPosition;
+    theWindow->lastY = yPosition;
+}
+
+GLfloat Window::getXChange() {
+    GLfloat temp = xChange;
+    xChange = 0;
+    return temp;
+}
+
+GLfloat Window::getYChange() {
+    GLfloat temp = yChange;
+    yChange = 0;
+    return temp;
 }
--- src/Window.h
+++ src/Window.h
@@ -16,16 +16,32 @@ public:
     GLint getBufferWidth() const { return bufferWidth; }
     GLint getBufferHeight() const { return bufferHeight; }
 
+    bool* getKeys() { return keys; }
+    GLfloat getXChange();
+    GLfloat getYChange();
+
     bool getShouldClose() const { return glfwWindowShouldClose(window); }
     void swapBuffers() { glfwSwapBuffers(window); }
 
     ~Window();
 private:
-    GLFWwindow* window;
+    GLFWwindow* window{nullptr};
+
     GLint width;
     GLint height;
-    GLint bufferWidth;
-    GLint bufferHeight;
+    GLint bufferWidth{0};
+    GLint bufferHeight{0};
+
+    bool keys[1024] = {false};
+
+    GLfloat lastX{0};
+    GLfloat lastY{0};
+    GLfloat xChange{0};
+    GLfloat yChange{0};
+
+    bool mouseFirstMove{true};
+
+    void createCallbacks();
+    static void handleKeys(GLFWwindow* window, int key, int code, int action, int mode);
+    static void handleMouse(GLFWwindow* window, double xPosition, double yPosition);
 };
-
-
